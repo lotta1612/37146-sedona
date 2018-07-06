e4e4499394819826dda62fd16f80abb51183c3d4
lotta1612 <strelchonok1@yandex.ru>
2018-07-06 15:43:35 +0300

    Ещё правки

---
+++ js/external.js
@@ -0,0 +1,7 @@
+var link = document.querySelector(".search-header");
+var popup = document.querySelector(".modal-form");
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal-form-show");
+});
