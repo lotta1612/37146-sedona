e7849fc59a2863c66467c7ed66e25fe791ea3898
lotta1612 <strelchonok1@yandex.ru>
2018-07-11 01:16:45 +0300

    Правки после финальной проверки

--- js/external.js
+++ js/external.js
@@ -3,5 +3,5 @@ var popup = document.querySelector(".modal-form");
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popup.classList.toggle("modal-form-show");
+  popup.classList.toggle("modal-form-hidden");
 });
