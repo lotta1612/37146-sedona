5b4a27a6b1acbe0599c283fc9d50cd9f086cb666
lotta1612 <strelchonok1@yandex.ru>
2018-07-13 15:19:51 +0300

    добавила анимацию ошибки

--- js/external.js
+++ js/external.js
@@ -1,7 +1,17 @@
 var link = document.querySelector(".search-header");
 var popup = document.querySelector(".modal-form");
+var form = document.querySelector(".modal-form");
+var arrival = form.querySelector("[name=hotel-arrival]");
+var departure = form.querySelector("[name=hotel-departure]");
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-form-hidden");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+  }
 });
