83718780f008c47e53f5283521b883ccd0bc403d
lotta1612 <strelchonok1@yandex.ru>
2018-07-13 20:46:53 +0300

    FINAL

--- js/external.js
+++ js/external.js
@@ -6,9 +6,8 @@ var departure = form.querySelector("[name=hotel-departure]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("modal-form-hidden");  
+  popup.classList.toggle("modal-form-hidden");
   popup.classList.toggle("modal-show");
-
 });
 
 form.addEventListener("submit", function (evt) {
